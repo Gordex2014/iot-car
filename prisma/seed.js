46050ba6e00c627d30d8f4c0e3e5a923d859fe45
Gordex2014 <alvaromsd_11@hotmail.com>
2022-03-17 00:42:33 -0400

    Seed admin script added

---
+++ prisma/seed.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const argon = require('argon2');
+
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: 'admin@admin.com' },
+    update: {},
+    create: {
+      email: 'admin@admin.com',
+      firstName: 'Admin',
+      lastName: 'Admin',
+      password: await argon.hash('admin'),
+      username: 'admin',
+      authProvider: 'LOCAL',
+      isActive: true,
+    },
+  });
+  console.log(admin);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
